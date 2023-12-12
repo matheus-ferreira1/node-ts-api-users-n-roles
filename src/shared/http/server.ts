937d23439241e07ca5719e6cbdc27f27b14a325f
Matheus Ferreira <matheus.fer33@gmail.com>
2023-12-12 20:51:08 -0300

    typeorm and database config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,37 +1,12 @@
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from "express";
+import "reflect-metadata";
 import "express-async-errors";
-import cors from "cors";
-import swaggerUi from "swagger-ui-express";
 
-import swaggerFile from "../../swagger.json";
-import { AppError } from "@shared/errors/AppError";
-import { routes } from "./routes";
+import { app } from "./app";
+import { dataSource } from "@shared/typeorm";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(routes);
-
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: "error",
-        message: error.message,
-      });
-    }
-    console.log(error);
-    return response.status(500).json({
-      status: "error",
-      message: "Internal server error",
-    });
-  },
-);
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server started on port ${process.env.PORT}! 🏆`);
+dataSource.initialize().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}!`);
+  });
 });
