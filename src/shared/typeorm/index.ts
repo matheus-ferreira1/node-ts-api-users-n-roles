bb7b96858e2a2f6fd5e9b7c57d9799015a57af93
Matheus Ferreira <matheus.fer33@gmail.com>
2023-12-12 21:14:44 -0300

    create role entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,9 +2,11 @@ import { DataSource } from "typeorm";
 
 import { CreateRolesTable1702425584960 } from "./migrations/1702425584960-CreateRolesTable";
 
+import { Role } from "@roles/entities/Role";
+
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1702425584960],
 });
