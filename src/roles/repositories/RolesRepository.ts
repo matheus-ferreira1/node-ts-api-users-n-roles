ad9b3e707dd22965cca9f8762ace0989e7dff569
Matheus Ferreira <matheus.fer33@gmail.com>
2023-12-12 21:31:32 -0300

    Refactor role repository to use typeorm methods

--- src/roles/repositories/RolesRepository.ts
+++ src/roles/repositories/RolesRepository.ts
@@ -1,15 +1,32 @@
+import { Repository } from "typeorm";
+
+import { dataSource } from "@shared/typeorm";
+
 import { Role } from "@roles/entities/Role";
 
 type CreateRoleDTO = {
   name: string;
 };
 
+export type PaginateParams = {
+  page: number;
+  skip: number;
+  take: number;
+};
+
+export type RolesPaginateProperties = {
+  per_page: number;
+  total: number;
+  current_page: number;
+  data: Role[];
+};
+
 export class RolesRepository {
-  private roles: Role[];
+  private repository: Repository<Role>;
   private static INSTANCE: RolesRepository;
 
   private constructor() {
-    this.roles = [];
+    this.repository = dataSource.getRepository(Role);
   }
 
   // Design pattern Singleton: Acima estou criando uma instância da classe RolesRepository (uma instancia dela mesma) e armazenando na variável estática INSTANCE. Colocando o construtor como private, eu garanto que não será possível criar uma instância da classe fora dela mesma. Para criar uma instância, eu preciso chamar o método getInstance(), que verifica se já existe uma instância da classe. Se já existir, ele retorna a instância já criada. Se não existir, ele cria uma nova instância e retorna.
@@ -21,25 +38,44 @@ export class RolesRepository {
     return RolesRepository.INSTANCE;
   }
 
-  create({ name }: CreateRoleDTO): Role {
-    const role = new Role();
-
-    Object.assign(role, {
-      name,
-      created_at: new Date(),
-    });
-
-    this.roles.push(role);
-
-    return role;
+  async create({ name }: CreateRoleDTO): Promise<Role> {
+    const role = this.repository.create({ name });
+    return this.repository.save(role);
   }
 
-  findAll(): Role[] {
-    return this.roles;
+  async save(role: Role): Promise<Role> {
+    return this.repository.save(role);
   }
 
-  findByName(name: string): Role | undefined {
-    const role = this.roles.find(role => role.name === name);
-    return role;
+  async delete(role: Role): Promise<void> {
+    await this.repository.remove(role);
+  }
+
+  async findAll({
+    page,
+    skip,
+    take,
+  }: PaginateParams): Promise<RolesPaginateProperties> {
+    const [roles, count] = await this.repository
+      .createQueryBuilder()
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+
+    const result = {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: roles,
+    };
+    return result;
+  }
+
+  async findByName(name: string): Promise<Role | null> {
+    return this.repository.findOneBy({ name });
+  }
+
+  async findById(id: string): Promise<Role | null> {
+    return this.repository.findOneBy({ id });
   }
 }
