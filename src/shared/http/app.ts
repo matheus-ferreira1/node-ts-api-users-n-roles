5b47e206ac842eb491abe2e0309bb2877b8533b7
Matheus Ferreira <matheus.fer33@gmail.com>
2023-12-18 22:20:38 -0300

    added input validation for roles

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
+import { errors } from "celebrate";
 
 import { routes } from "./routes";
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
