389c9fc2d9f649fae87e04ac4b6e4907af6543ca
Matheus Ferreira <matheus.fer33@gmail.com>
2023-12-20 23:34:17 -0300

    create migration AddRoleIdToUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,10 +4,15 @@ import { CreateRolesTable1702425584960 } from "./migrations/1702425584960-Create
 
 import { Role } from "@roles/entities/Role";
 import { CreateUsersTable1703125165250 } from "./migrations/1703125165250-CreateUsersTable";
+import { AddRoleIdToUsersTable1703125554269 } from "./migrations/1703125554269-AddRoleIdToUsersTable";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
   entities: [Role],
-  migrations: [CreateRolesTable1702425584960, CreateUsersTable1703125165250],
+  migrations: [
+    CreateRolesTable1702425584960,
+    CreateUsersTable1703125165250,
+    AddRoleIdToUsersTable1703125554269,
+  ],
 });
