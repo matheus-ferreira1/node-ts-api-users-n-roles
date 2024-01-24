93ea4649099df17828764dfbbf3ba079493d0f35
Matheus Ferreira <matheus.fer33@gmail.com>
2024-01-23 22:12:32 -0300

    create user entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,15 +1,16 @@
 import { DataSource } from "typeorm";
 
-import { CreateRolesTable1702425584960 } from "./migrations/1702425584960-CreateRolesTable";
-
 import { Role } from "@roles/entities/Role";
+import { User } from "@users/entities/User";
+
+import { CreateRolesTable1702425584960 } from "./migrations/1702425584960-CreateRolesTable";
 import { CreateUsersTable1703125165250 } from "./migrations/1703125165250-CreateUsersTable";
 import { AddRoleIdToUsersTable1703125554269 } from "./migrations/1703125554269-AddRoleIdToUsersTable";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1702425584960,
     CreateUsersTable1703125165250,
