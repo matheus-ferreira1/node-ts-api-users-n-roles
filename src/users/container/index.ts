2e9fec78efabc5753447f91e30a1b9abdb5b629f
Matheus Ferreira <matheus.fer33@gmail.com>
2024-01-23 23:05:56 -0300

    added createUser route

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -2,8 +2,11 @@ import { container } from "tsyringe";
 
 import { IUsersRepository } from "@users/repositories/IUsersRepository";
 import { UsersRepository } from "@users/repositories/UsersRepository";
+import { CreateUserController } from "@users/useCases/createUser/CreateUserController";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository,
 );
+
+container.registerSingleton("CreateUserController", CreateUserController);
