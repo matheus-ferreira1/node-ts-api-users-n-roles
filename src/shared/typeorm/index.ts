fcd591ad66f06143b7163c83ae476c2d4720ddea
Matheus Ferreira <matheus.fer33@gmail.com>
2024-02-25 21:26:48 -0300

    Create migration CreateRefreshTokensTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -6,6 +6,7 @@ import { User } from "@users/entities/User";
 import { CreateRolesTable1702425584960 } from "./migrations/1702425584960-CreateRolesTable";
 import { CreateUsersTable1703125165250 } from "./migrations/1703125165250-CreateUsersTable";
 import { AddRoleIdToUsersTable1703125554269 } from "./migrations/1703125554269-AddRoleIdToUsersTable";
+import { CreateRefreshTokensTable1708906821352 } from "./migrations/1708906821352-CreateRefreshTokensTable";
 
 export const dataSource = new DataSource({
   type: "sqlite",
@@ -15,5 +16,6 @@ export const dataSource = new DataSource({
     CreateRolesTable1702425584960,
     CreateUsersTable1703125165250,
     AddRoleIdToUsersTable1703125554269,
+    CreateRefreshTokensTable1708906821352,
   ],
 });
